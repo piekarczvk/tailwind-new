87a393a6fb12fba34b55d5cac3630fa5549480e1
Magdalena Piekarczyk <piekarczvk@gmail.com>
2023-09-26 23:29:15 +0400

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-dark": "#121139",
+        "text-light": "#F8F8F8",
+        "pink-light": "#9130F4",
+        "blue-light": "#4646F9",
+      },
+    },
+  },
+  plugins: [],
+};
